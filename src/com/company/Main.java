ca194e379e13a84a509d7784b5899cb7d87a0102
Дмитро Жорник <dmytro.zhornyk@ukma.edu.ua>
2020-04-19 01:18:23 +0300

    Updated stylesheet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,15 @@ import com.company.view.MainFrame;
 import com.company.view.ObjectsPannel;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class Main {
 
     public static MainFrame mainFrame;
     public static String filepath;
     public static ArrayList<Group> groups;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel ("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         mainFrame = new MainFrame();
         mainFrame.setVisible(true);
         mainFrame.setSize(800,600);
